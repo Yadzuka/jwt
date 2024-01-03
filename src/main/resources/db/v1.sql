d1743132a692dfb53f1c6b0af5a6e0ca7324dfe3
Yadzuka <yadzuka@eustrosoft.org>
2024-01-03 08:51:17 +0300

    Added simple jwt realization with spring and postgres

--- src/main/resources/db/v1.sql
+++ src/main/resources/db/v1.sql
@@ -1,4 +1,4 @@
-create table default.users (
+create table users (
     id bigserial,
     username varchar(30) not null unique,
     password varchar(80) not null,
@@ -6,13 +6,13 @@ create table default.users (
     primary key (id)
 );
 
-create table default.role(
+create table role(
     id serial,
     name varchar(50) not null,
     primary key (id)
 );
 
-create table default.user_roles(
+create table user_roles(
     user_id bigint not null,
     role_id int not null,
     primary key (user_id, role_id),
@@ -20,12 +20,12 @@ create table default.user_roles(
     foreign key (role_id) references role(id)
 );
 
-insert into default.role(name) values
+insert into role(name) values
 ('ROLE_USER'), ('ROLE_ADMIN');
 
-insert into default.users(username, password, email) values
+insert into users(username, password, email) values
 ('user','$2a$10$tecGLPkUjM/L4RA/o2Os/.FOwaB1AtGV.kx.iIBgAAhD98aXvozE.','user@mail.ru'),
 ('admin', '$2a$10$cgSbX85roS0AjBDJzu3eZuFHnhCmlcgqGTPVz.TLG2eY.7DmWJUqK', 'admin@mail.ru');
 
-insert into default.user_roles(user_id, role_id) values
+insert into user_roles(user_id, role_id) values
 (1, 1), (2,2);
