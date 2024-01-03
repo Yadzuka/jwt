f3412158501c1a4ae8c221acfe1336ef40d014cc
Yadzuka <yadzuka@eustrosoft.org>
2024-01-03 08:51:53 +0300

    Added filter and http requests collection

---
+++ src/main/java/ru/yadzuka/config/JwtAuthFilter.java
@@ -0,0 +1,52 @@
+package ru.yadzuka.config;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.SignatureException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import ru.yadzuka.utils.JwtTokenUtils;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private final JwtTokenUtils tokenUtils;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String header = request.getHeader("Authorization");
+        String username = null;
+        String jwt = null;
+
+        if (header != null && header.startsWith("Bearer")) {
+            jwt = header.substring(7);
+            try {
+                username = tokenUtils.getUsername(jwt);
+            } catch (ExpiredJwtException | SignatureException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                    username,
+                    null,
+                    tokenUtils.getRoles(jwt).stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList())
+            );
+            SecurityContextHolder.getContext().setAuthentication(token);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
